1f719d916654794bcd5b8cc716ca7e5305651029
Diana Vidal <dvidaal@gmail.com>
2023-03-02 20:52:20 +0100

    Create useState for each button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,22 @@
-interface ButtonProps {
-  text: string;
-}
+import { useState } from "react";
 
-const Button = ({ text }: ButtonProps): JSX.Element => {
-  return <button>{text}</button>;
+interface ButtonProps {
+  name: string;
+}
+const Button = ({ name: text }: ButtonProps): JSX.Element => {
+  const [, setInitialValue] = useState("");
+
+  const handleValueChange = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+
+    setInitialValue(event.currentTarget.name);
+  };
+
+  return (
+    <button name={text} onClick={handleValueChange}>
+      {text}
+    </button>
+  );
 };
 
 export default Button;
